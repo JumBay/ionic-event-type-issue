778ccd30acc29c0ea172d982d5dfc74f7b4b5bd6
Jean-Baptiste Malatrasi <jb.malatrasi@gmail.com>
2021-11-18 17:01:46 +0100

    Added breaking code

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ItemReorderEventDetail } from '@ionic/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
 
 }
